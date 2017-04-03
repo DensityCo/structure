b37cbe8e95b24523e11af58c381dfc8c6c966c1d
Gus Cost <guscost@gmail.com>
2017-04-03 13:51:29 -0400

    code: export function?

--- scripts/init.js
+++ scripts/init.js
@@ -1,3 +1,9 @@
-module.exports = () => {
+module.exports = function(
+  appPath,
+  appName,
+  verbose,
+  originalDirectory,
+  template
+) {
   console.log('Edit init/index.js to add init tasks!');
 }

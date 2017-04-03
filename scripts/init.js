be8de44c119bd25745527cea2cb144b1f3f4f590
Gus Cost <guscost@gmail.com>
2017-04-03 14:00:55 -0400

    code: logic to update package json

--- scripts/init.js
+++ scripts/init.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function(
   appPath,
   appName,
@@ -5,5 +8,25 @@ module.exports = function(
   originalDirectory,
   template
 ) {
-  console.log('Edit init/index.js to add init tasks!');
+  console.log('Update package.json...')
+  const appPackage = require(path.join(appPath, 'package.json'));
+
+  // Copy over some of the devDependencies
+  appPackage.dependencies = appPackage.dependencies || {};
+  appPackage.devDependencies = appPackage.devDependencies || {};
+
+  // Setup the script rules
+  appPackage.scripts = {
+    start: 'react-scripts start',
+    build: 'react-scripts build',
+    test: 'react-scripts test --env=jsdom',
+    eject: 'react-scripts eject',
+  };
+
+  fs.writeFileSync(
+    path.join(appPath, 'package.json'),
+    JSON.stringify(appPackage, null, 2)
+  );
+
+  console.log('Edit init/index.js to add more init tasks!');
 }

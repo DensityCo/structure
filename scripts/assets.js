490f26c93051b9387b9d0e22aa411a8afa24828c
Gus Cost <guscost@gmail.com>
2017-04-08 11:29:53 -0400

    code: promisify assets

--- scripts/assets.js
+++ scripts/assets.js
@@ -8,27 +8,30 @@ function copy(
   index = null,
   dest = './dist'
 ) {
-  const indexPath = `${dest}/index.html`;
-  utilities.ensureDirectoryExistence(indexPath);
-  if (typeof(index) === String) { index = fs.readFileSync(index); }
-  fs.writeFileSync(indexPath, index || `<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>App</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="/app.css">
-  </head>
-  <body>
-    <div id="react-mount"></div>
-    <script src="/app.js"></script>
-  </body>
-</html>`);
+  return new Promise((resolve, reject) => {
+    const indexPath = `${dest}/index.html`;
+    utilities.ensureDirectoryExistence(indexPath);
+    if (typeof(index) === String) { index = fs.readFileSync(index); }
+    fs.writeFileSync(indexPath, index || `<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>App</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+      <link rel="stylesheet" href="/app.css">
+    </head>
+    <body>
+      <div id="react-mount"></div>
+      <script src="/app.js"></script>
+    </body>
+  </html>`);
 
-  paths.forEach((path) => utilities.copyRecursiveSync(path, `${dest}/assets`));
-  console.log(chalk.gray('Assets ready!'));
+    paths.forEach((path) => utilities.copyRecursiveSync(path, `${dest}/assets`));
+    console.log(chalk.gray('Assets ready!'));
+    resolve();
+  });
 }
 
 module.exports = {

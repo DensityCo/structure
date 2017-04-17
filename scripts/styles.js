25699c23e7730f88b08710a296f863e758d30326
Gus Cost <guscost@gmail.com>
2017-04-17 11:05:49 -0400

    code: autoprefixer

--- scripts/styles.js
+++ scripts/styles.js
@@ -2,6 +2,9 @@ const chalk = require('chalk');
 const fs = require('fs');
 const sass = require('node-sass');
 
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
+
 let _main = null;
 let _paths = null;
 let _bundle = null;
@@ -14,13 +17,18 @@ function configure(main, paths, bundle) {
 
 function compile(main = _main, paths = _paths, bundle = _bundle) {
   return new Promise((resolve, reject) => {
-    const result = sass.renderSync({
+    const css = sass.renderSync({
       file: main,
       includePaths: paths
     });
-    fs.writeFileSync(bundle, result.css);
-    console.log(chalk.gray('Styles ready!'));
-    resolve();
+    postcss([ autoprefixer ]).process(css).then(function (result) {
+        result.warnings().forEach(function (warn) {
+          console.log(chalk.red(warn.toString()));
+        });
+        fs.writeFileSync(bundle, result.css);
+        console.log(chalk.gray('Styles ready!'));
+        resolve();
+    });
   });
 }
 

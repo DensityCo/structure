51d859799b8010d13d566a1f6f3d18a4d09b5839
Gus Cost <guscost@gmail.com>
2017-05-18 17:22:28 -0400

    code: fix typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const sass = require('./scripts/styles_sass');
 const typescript = require('./scripts/transpiler_typescript');
 const babel = require('./scripts/transpiler_babel');
 const webpack = require('./scripts/bundler_webpack');
-const browserify = require('./scripts/bundler_browserify');s
+const browserify = require('./scripts/bundler_browserify');
 const start = require('./scripts/start');
 const build = require('./scripts/build');
 

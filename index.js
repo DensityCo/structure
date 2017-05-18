a432bc2ce73cfe54d44d147195e89f03b3836789
Gus Cost <guscost@gmail.com>
2017-05-18 16:50:26 -0400

    code: add assets to api

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const start = require('./scripts/start');
-const build = require('./scripts/build');
+const assets = require('./scripts/assets');
 
 const sass = require('./scripts/styles_sass');
+
 const typescript = require('./scripts/transpiler_typescript');
 const babel = require('./scripts/transpiler_babel');
+
 const webpack = require('./scripts/bundler_webpack');
 const browserify = require('./scripts/bundler_browserify');
 
+const start = require('./scripts/start');
+const build = require('./scripts/build');
+
 module.exports = {
-  start: start,
-  build: build,
+  assets: assets,
 
   sass: sass,
+
   typescript: typescript,
   babel: babel,
+
   webpack: webpack,
   browserify: browserify,
+
+  start: start,
+  build: build,
 };

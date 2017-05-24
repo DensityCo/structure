814520acf6b420c284ad33ce63ff74875dd42f98
Gus Cost <guscost@gmail.com>
2017-05-23 20:04:08 -0400

    code: promisify init script?

--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,5 @@
 const chalk = require('chalk');
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const utilities = require('./utilities');
 
@@ -27,27 +27,29 @@ module.exports = function(
     eject: 'react-scripts eject',
   };
 
-  fs.writeFileSync(
+  fs.writeFile(
     path.join(appPath, 'package.json'),
     JSON.stringify(appPackage, null, 2)
-  );
+  ).then(() => {
+    // Copy structure template into the project
+    const templateReadStream = fs.createReadStream(path.resolve(__dirname, path.join('..', 'template', 'structure.js')))
+    const templateWriteStream = fs.createWriteStream(path.resolve(__dirname, path.join('..', '..', '..', '..', 'structure.js')))
+    templateReadStream.on('error', err => console.log("Template copy read stream error:", err))
+    templateWriteStream.on('error', err => console.log("Template copy write stream error:", err))
+    templateReadStream.pipe(templateWriteStream);
 
-  // Copy structure template into the project
-  const templateReadStream = fs.createReadStream(path.resolve(__dirname, path.join('..', 'template', 'structure.js')))
-  const templateWriteStream = fs.createWriteStream(path.resolve(__dirname, path.join('..', '..', '..', '..', 'structure.js')))
-  templateReadStream.on('error', err => console.log("Template copy read stream error:", err))
-  templateWriteStream.on('error', err => console.log("Template copy write stream error:", err))
-  templateReadStream.pipe(templateWriteStream);
-
-  // Copy template files
-  console.log(chalk.gray('Copy template...'));
-  var templatePath = template ? path.resolve(originalDirectory, template) : path.join(ownPath, 'template');
-  if (fs.existsSync(templatePath)) {
-    utilities.copyRecursiveSync(templatePath, appPath);
-  } else {
-    console.error('Could not locate supplied template: ' + chalk.green(templatePath));
-    return;
-  }
-
-  console.log(chalk.gray('Edit init/index.js to add more init tasks!'));
+    // Copy template files
+    console.log(chalk.gray('Copy template...'));
+    const templatePath = template ? path.resolve(originalDirectory, template) : path.join(ownPath, 'template');
+    return fs.exists(templatePath);
+  }).then(exists => {
+    if (exists) {
+      return utilities.copyRecursive(templatePath, appPath);
+    } else {
+      console.error('Could not locate supplied template: ' + chalk.green(templatePath));
+      return;
+    }
+  }).then(() => {
+    console.log(chalk.gray('Edit init/index.js to add more init tasks!'));
+  });
 }
